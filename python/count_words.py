8f4e07becdfac983373c27754f96a1d855441c45
aemc <aemlcruz@gmail.com>
2018-10-28 12:00:18 -0400

    Add comments

--- python/count_words.py
+++ python/count_words.py
@@ -1,6 +1,10 @@
 from collections import Counter
 
 def count_words(s, n):
+    """Implement a function count_words() in Python that takes as input a string s and a number n, and 
+    returns the n most frequently-occuring words in s. The return value should be a list of tuples - 
+    the top n words paired with their respective counts [(, ), (, ), ...], sorted in descending count 
+    order."""
     return Counter(s.split()).most_common(n)
 
 def test_run():
